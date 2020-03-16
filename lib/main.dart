f5d457a4fbc3d6d1e6fffa38e2323ec818b82e0e
FahimMuntashir <fahim.tech.me@gmail.com>
2020-03-16 17:34:56 +0600

    loacl position

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MaterialApp(
-      new HomePage(),
+     home: new HomePage(),
   debugShowCheckedModeBanner: false,
 ));
 
@@ -11,26 +12,50 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Offset> _points;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: new Container(
+        child: new GestureDetector(
+
+          onPanUpdate:(DragUpdateDetails details){
+
+            setState(() {
+              RenderBox object = context.findRenderObject();
+              Offset _loclposition = object.globalToLocal(details.globalPosition);
+              _points = new List.from(_points)..add(_loclposition);
+            });
+          } ,
+        ),
 
       ),
     );
   }
 }
-class Signature extends CustomClipper<Path>{
+class Signature extends CustomPainter{
+  List<Offset> points;
+  Signature({this.points});
+
   @override
-  Path getClip(Size size) {
-    // TODO: implement getClip
-    return null;
+  void paint(Canvas canvas, Size size) {
+
+    Paint paint = new Paint()
+    ..color = Colors.black
+    ..strokeCap = StrokeCap.round
+    ..strokeWidth = 5.0;
+    for(int i = 0; i<points.length-1;i++)
+      {
+        if(points[i] !=null && points[i+1] != null)
+          {
+            canvas.drawLine(points[i], points[i+1], paint);
+          }
+      }
   }
 
   @override
-  bool shouldReclip(Signature oldClipper) {
-    // TODO: implement shouldReclip
-    return null;
+  bool shouldRepaint(Signature oldDelegate) => oldDelegate.points != points;
+
+
   }
 
-}
